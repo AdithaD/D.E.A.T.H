13a8fc0430dbe65b092b73d9d93d29d900d392b2
Ryan <ryanwp526@gmail.com>
2022-04-02 22:09:19 +1100

    Uses more efficient bfs pathing now

--- Scripts/EnemyScripts/EnemyAI/GruntAI.gd
+++ Scripts/EnemyScripts/EnemyAI/GruntAI.gd
@@ -1,14 +1,15 @@
 extends Node
 
 export (int) var min_dist = 5
-const a_start_path = "res://Scripts/Pathing/AStar.gd"
-const a_star_class = preload(a_start_path)
+const bfs_path = "res://Scripts/Pathing/BFS.gd"
+const bfs_class = preload(bfs_path)
 
 func get_move(enemy: Enemy):
 	var god = get_tree().root.get_child(0)
-	var a_star = a_star_class.new()
-	a_star.init(1000, god.get_obstacle_locations())
-	return a_star.find_path(enemy.grid_position, Vector2(0, 2))
+	var bfs = bfs_class.new()
+	bfs.init(100, god.get_obstacle_locations())
+#	return []
+	return bfs.find_path(enemy.grid_position, Vector2(0, 2))
 	
 #func get_action(enemy: Enemy):
 	
