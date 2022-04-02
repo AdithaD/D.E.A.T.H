994a42227da00e36b86f57b8d70130f1b95f6469
Ryan <ryanwp526@gmail.com>
2022-04-02 22:44:24 +1100

    Simple code to actually move enemy

--- Scripts/EnemyScripts/Enemy.gd
+++ Scripts/EnemyScripts/Enemy.gd
@@ -12,19 +12,21 @@ var health
 var abilities = []
 var ai
 var grid_position = Vector2(0, 0)
+var god
 
 func _ready():
 	health = max_health
 	abilities = $Abilities.get_children()
 	ai = $AI
+	god = get_tree().root.get_child(0)
 	
 
 func new_turn():
-	var move = [grid_position]
 	if(ai.has_method("get_move")):
-		move = ai.get_move()
-	print(move)
-
+		var move = ai.get_move()
+		grid_position = move[-1]
+		position = god.get_obstacle_tilemap().map_to_world(grid_position)
+		
 
 
 func _on_World_new_turn():
