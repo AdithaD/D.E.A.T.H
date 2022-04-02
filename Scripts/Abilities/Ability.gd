193620dc15ed49cffc49bd28024428a679ccf2f9
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-02 19:13:26 +1100

    added ability systema and GunShoot ability

---
+++ Scripts/Abilities/Ability.gd
@@ -0,0 +1,32 @@
+extends Node
+class_name Ability
+
+export (String) var ability_name = "Ability"
+export (String) var ability_description = "Ability Description"
+
+export (int) var action_cost = 1
+export (int) var cooldownDuration = 1
+
+enum TARGET_TYPE { player, enemy, cover, tile }
+export(TARGET_TYPE) var target_type = TARGET_TYPE.enemy
+
+
+var cooldown = 0
+
+func new_turn():
+	if(cooldown > 0):
+		cooldown -= 1;
+
+func can_use_ability(player):
+	return player.actionPoints <= 0 || cooldown != 0
+
+
+func use_ability(player, target):
+	if (can_use_ability(player)) :
+		_use_ability(player, target)
+
+		player.spendActionPoint(action_cost)
+		cooldown = cooldownDuration
+
+func _use_ability(_player, _target):
+	pass
--- Scripts/PlayerUnit.gd
+++ Scripts/PlayerUnit.gd
@@ -1,26 +1,34 @@
 extends Node2D
-
+class_name PlayerUnit
 
 export (int) var max_health
 export (int) var actionPointsPerTurn
+export (int) var tilesPerMove
+export (int) var cooldown
 
 var health = max_health
+var actionPoints = actionPointsPerTurn
 
-var abilities = []
+var abilities = [] 
+
+var grid_position = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = max_health
-	print('super')
 	
 	abilities = $Abilities.get_children()
+			
+	new_turn()
 	pass # Replace with function body.
 
+func new_turn():
+	actionPoints = actionPointsPerTurn
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spendActionPoint(action_cost: int):
+	actionPoints -= 1
+	pass
 
-func shoot():
-	print('ding')
+func moveTo(x: int, y: int):
 	pass
