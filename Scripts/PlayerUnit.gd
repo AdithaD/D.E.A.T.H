26ebd9f32219f934dfa3d5dd6b9391186ac2f6a4
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-02 16:07:43 +1100

    added scaffolding for abilities

--- Scripts/PlayerUnit.gd
+++ Scripts/PlayerUnit.gd
@@ -2,13 +2,18 @@ extends Node2D
 
 
 export (int) var max_health
+export (int) var actionPointsPerTurn
 
 var health = max_health
+
+var abilities = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = max_health
 	print('super')
+	
+	abilities = $Abilities.get_children()
 	pass # Replace with function body.
 
 
