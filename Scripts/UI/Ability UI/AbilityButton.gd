bd8591214325c26f58c9dd0ea64c7912c2818357
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-03 06:32:56 +1000

    refactored script directory

--- Scripts/UI/Ability UI/AbilityButton.gd
+++ Scripts/UI/Ability UI/AbilityButton.gd
@@ -1,14 +1,12 @@
-extends "res://Scripts/Controller/God.gd"
+extends TextureButton
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var ability_button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if ability_button == null:
+		print('this ability button doesn\'t have an ability')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
