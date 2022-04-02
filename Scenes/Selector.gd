440d3cf3ba4fe8c52b6d6d989130c9a325571ba3
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-02 21:26:34 +1100

    added selector overlay

--- Scenes/Selector.gd
+++ Scenes/Selector.gd
@@ -1,7 +1,12 @@
-extends Control
+extends Node2D
 
 
 var player
+export (NodePath) var obstacle_map_path
+export (NodePath) var floor_map_path
+onready var obstacle_tile_map : TileMap = get_node(obstacle_map_path)
+onready var floor_tile_map : TileMap = get_node(floor_map_path)
+
 
 func _ready():
 	
@@ -9,4 +14,9 @@ func _ready():
 
 
 func _process(delta):
-	print(get_global_mouse_position())
+	var mouse_pos = get_global_mouse_position()
+	var tile = obstacle_tile_map.world_to_map(mouse_pos)
+	var snapped_pos = obstacle_tile_map.map_to_world(tile)
+	
+	var new_pos = Vector2(snapped_pos.x, snapped_pos.y + 8)
+	$Sprite.global_position = new_pos
--- Scripts/Controller/God.gd
+++ Scripts/Controller/God.gd
@@ -3,8 +3,9 @@ extends Node
 #export (Array, NodePath) var player_unit_node_paths
 #onready var player_units = load_nodes(player_unit_node_paths)
 export (NodePath) var obstacle_map_path
+export (NodePath) var floor_map_path
 onready var obstacle_tile_map = get_node(obstacle_map_path)
-
+onready var floor_tile_map = get_node(floor_map_path)
 #func _ready():
 #	print(get_obstacle_locations())
 #
