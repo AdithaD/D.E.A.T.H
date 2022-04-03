91a03e0cc47ba62f248ca2f9e83ba2231a57df87
Ryan <ryanwp526@gmail.com>
2022-04-03 22:11:29 +1000

    Add shoot AOE ability

--- Scripts/EnemyAbilities/EnemyShootAOE.gd
+++ Scripts/EnemyAbilities/EnemyShootAOE.gd
@@ -1,27 +1,48 @@
 extends EnemyAbility
 
 export (int) var damage = 1
-export (int) var penetration = 0
+export (int) var area_of_effect = 1
 export (int) var shoot_range = 10
 
 func _use_ai_ability(source):
 	#randomize()
-	print('enemy shooting')
 	var target = _generate_target(source)
 	if target != null:
 		var god = get_tree().root.get_child(0)
-		var rand = god.get_hit_chance(source.grid_position, target.grid_position, penetration, !target.can_cover, target.is_marked)
-		if(randf() < rand):
-			target.take_damage(damage)
+		var obstacle_tilemap = god.get_obstacle_tilemap()
+		var floor_tilemap = god.get_floor_tilemap()
+		
+		var tiles_affected = generateAoE(target)
+		
+		for tile in tiles_affected:
+			if obstacle_tilemap.get_cellv(tile) != -1:
+				obstacle_tilemap.set_cellv(tile, obstacle_tilemap.tile_set.find_tile_by_name("rubble"))
+		
+		var units = god.get_enemy_nodes() + god.get_player_nodes()
+		for unit in units:
+			if tiles_affected.find(unit.grid_position) != -1:
+				unit.take_damage(damage)
 	
 	finish_doing()
 	
+# returns tile todo: make this actually find good target
 func _generate_target(source):
 	var players = ai.get_players_in_range(shoot_range, source.grid_position)
 	
 	if(players.size() > 0):
 		var index  = randi() % players.size()
 		
-		return players[index]
+		return players[index].grid_position
 	
 	return null
+
+
+func generateAoE(target):
+	var pos = target
+	var list = []
+	
+	for i in range(-area_of_effect, area_of_effect + 1):
+		for j in range(-area_of_effect, area_of_effect + 1):
+			list.append(pos + Vector2(i, j))
+			
+	return list
