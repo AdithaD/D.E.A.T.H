8491e1ea2be06b1902cf3fd21cec1cdac9168f0b
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-03 21:04:03 +1000

    ability switching is better

--- Scripts/Abilities/AbilityLifecycleManager.gd
+++ Scripts/Abilities/AbilityLifecycleManager.gd
@@ -8,12 +8,17 @@ onready var primary_selector = get_node(selector_path)
 onready var ability_prompt = get_node(ability_prompt_path)
 
 func submit_ability(player, ability_index):
-	if get_children().size() == 0:
-		var life_cycle = ability_lifecyle_scene.instance()
-		add_child(life_cycle)
-
-		life_cycle.init(player, ability_index, primary_selector)
+	for child in get_children():
+			child.queue_free()
 			
-		ability_prompt.set_ability(life_cycle)
-		player.get_node("PlayerUI").bind_lifecycle(life_cycle)
-		life_cycle.start()
+
+	var life_cycle = ability_lifecyle_scene.instance()
+	add_child(life_cycle)
+
+	life_cycle.init(player, ability_index, primary_selector)
+		
+	ability_prompt.set_ability(life_cycle)
+	player.get_node("PlayerUI").bind_lifecycle(life_cycle)
+	life_cycle.start()
+
+		
