e62f1675c74c38a5dce7547613ba327e61d73bfa
Ryan <ryanwp526@gmail.com>
2022-04-03 18:21:04 +1000

    Smooth player movement implemented

--- Scripts/PlayerUnits/PlayerUnit.gd
+++ Scripts/PlayerUnits/PlayerUnit.gd
@@ -7,6 +7,9 @@ export (int) var tilesPerMove
 
 export (bool) var can_cover = true
 
+export (float) var move_animation_time = 0.15
+var move_interval = 0.025
+
 var health
 var action_points = actionPointsPerTurn
 
@@ -46,9 +49,6 @@ func spend_action_points(action_cost: int):
 	emit_signal('update_attr')
 	pass
 
-func moveTo(x: int, y: int):
-	pass
-	
 func take_damage(dmg):
 	health -= dmg
 	emit_signal("update_attr")
@@ -56,14 +56,27 @@ func take_damage(dmg):
 func on_used_ability(index):
 	emit_signal("used_ability")
 
-func set_grid_position(new_grid):
-	grid_position = new_grid
-	position = god.grid_to_world(grid_position)
 	
 func apply_mark(turns):
 	if turns > mark_length:
 		mark_length = turns
-	is_marked = true 
+	is_marked = true
+	
+func do_move(path):
+	grid_position = path[-1]
+	print(grid_position)
+	for loc in path:
+		yield(world_move_to(god.grid_to_world(loc)), "completed")
+	
+
+func world_move_to(loc):
+	var diff = loc - position
+	var steps = int(move_animation_time/move_interval)
+	for i in range(0, steps):
+		yield(get_tree().create_timer(move_interval), "timeout")
+		position += diff/steps
+	
+	position = loc
 
 func _on_PlayerUnit_took_damage():
 	pass # Replace with function body.
