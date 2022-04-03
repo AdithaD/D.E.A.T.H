1208ed995661d80d024f1cc4349701ce5860d509
Ryan <ryanwp526@gmail.com>
2022-04-03 22:39:28 +1000

    Recon now marks

--- Scripts/EnemyScripts/EnemyAI/GruntAI.gd
+++ Scripts/EnemyScripts/EnemyAI/GruntAI.gd
@@ -19,7 +19,8 @@ func generate_turn(abilities):
 	
 	var other_ability = abilities[1]
 	if(abilities.size() > 2):
-		other_ability  = abilities[randi() % len(abilities)]
+		var other_abilities = abilities.slice(1,len(abilities) - 1)
+		other_ability  = other_abilities[randi() % len(other_abilities)]
 		
 	return [move_ability, other_ability]
 
