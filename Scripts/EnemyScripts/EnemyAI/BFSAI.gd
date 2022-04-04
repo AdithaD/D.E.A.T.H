a7ee01b736293ea4c9e814d75e318a9d84471afc
Ryan <ryanwp526@gmail.com>
2022-04-04 20:29:59 +1000

    Implement smarter generic shooting and marking

--- Scripts/EnemyScripts/EnemyAI/BFSAI.gd
+++ Scripts/EnemyScripts/EnemyAI/BFSAI.gd
@@ -41,6 +41,13 @@ func get_players_in_range(ability_range, pos):
 			in_range.append(player)
 	return in_range
 
+func get_civilians_in_range(ability_range, pos):
+	var in_range = []
+	for civilian in god.get_civilian_nodes():
+		if bfs.grid_distance(civilian.grid_position, pos) <= ability_range:
+			in_range.append(civilian)
+	return in_range
+
 func get_flying_players_in_range(ability_range, pos):
 	var in_range = []
 	for player in god.get_flying_player_nodes():
--- Scripts/EnemyScripts/EnemyAI/GruntAI.gd
+++ Scripts/EnemyScripts/EnemyAI/GruntAI.gd
@@ -9,3 +9,9 @@ func generate_turn(abilities):
 		other_ability  = other_abilities[randi() % len(other_abilities)]
 		
 	return [move_ability, other_ability]
+
+func evaulate_tile():
+	# + points if a target within skill range/closer to target
+	# + points for high acc to target
+	# - points for all enemies with high acc towards it
+	pass
