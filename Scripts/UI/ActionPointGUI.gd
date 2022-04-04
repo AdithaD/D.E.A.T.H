ca8765e88068e8d262cbcdd899293c7cd3fa13dd
Aditha Doratiyawa <indegamedev@gmail.com>
2022-04-05 01:57:02 +1000

    action points gui and colored hp bars

---
+++ Scripts/UI/ActionPointGUI.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+export (int) var segment_size = 5
+
+onready var actionbar = $ActionFull
+onready var actionbarempty = $ActionEmpty
+
+var max_action_points = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if(get_parent() and get_parent().get("max_health")):
+		max_action_points = get_parent().actions_points_per_turn
+	
+	actionbarempty.rect_size.x = max_action_points * segment_size
+	position.x = -(actionbarempty.rect_size.x / 2)
+	update()
+	
+func update():
+	if get_parent() and (get_parent().get("action_points") != null):
+		if(get_parent().action_points <= 0):
+			actionbar.visible = false
+			actionbarempty.visible = false
+		else:
+			actionbar.visible = true
+			actionbarempty.visible = true
+			actionbar.rect_size.x = get_parent().action_points * segment_size
+
+func _on_Unit_update_attr():
+	update()
+	pass
